ec8e13624f1c7cdea59e91caf06967d2f27f9630
Andrew Vickerman <andrew@Andrews-MacBook-Pro.local>
2019-05-15 11:01:52 -0400

    step 3

--- search.php
+++ search.php
@@ -4,14 +4,12 @@
  */
 ?>
 
-<div class="wrapper">
-  <div id="app">
-    <ol>
-      <li v-for="movie in movies">
-        {{ movie.title }}
-      </li>
-    </ol>
-  </div>
+<div id="app">
+  <ol>
+    <li v-for="movie in movies">
+      {{ movie.title }}
+    </li>
+  </ol>
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
