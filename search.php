41dbf5549c7186e5da2b59c6989a8b09abae5611
Andrew Vickerman <andrew@Andrews-MacBook-Pro.local>
2019-05-15 11:42:26 -0400

    -

--- search.php
+++ search.php
@@ -6,12 +6,19 @@
 
 <div id="app">
   <ol>
-    <li v-for="movie in movies" v-if="movie.title && movie.description">
-      <h6>{{ movie.title }}</h6>
+    <li v-for="movie in movies" v-if="movie.title || movie.description">
+      <h2 v-if="movie.title">
+        {{ movie.title }}
+      </h2>
+      <p v-if="movie.description">
+        {{ movie.description }}
+      </p>
     </li>
   </ol>
-  <input type="text" v-model="title">
-  <input type="text" v-model="description">
+  <input type="text" v-model="title" placeholder="Movie Title">
+  <br>
+  <textarea v-model="description" placeholder="Movie Description"></textarea>
+  <br>
   <button @click="addMovie">Add Movie</button>
 </div>
 
@@ -21,15 +28,35 @@
 var app = new Vue({
   el: '#app',
   data: {
+    title: '',
+    description: '',
     movies: [
       { 
         title: 'Chappie', 
         description: 'When a police droid, Chappie, is stolen and given new programming, he becomes the first robot with the ability to think and feel for himself.' 
       },
       {
-        title: ''
+        title: 'Lord of the Rings',
+        description: 'Peoples of Middle-earth, such as Men, Elves, Dwarves, and Hobbits, must overcome the dark power of Sauron by destroying the Ring that gives him power.'
+      },
+      {
+        title: 'When Kittens Attack',
+        description: 'Litterally just cute kittens pouncing on things.'
+      },
+      {
+        title: 'Star Wars: Episode V',
+        description: 'Talking frog convinces a boy to kill his dad'
       }
     ]
+  },
+  methods: {
+    addMovie(){
+      if(this.title || this.description){
+        this.movies.push({ title: this.title, description: this.description })
+        this.title = ''
+        this.description = ''
+      }
+    }
   }
 })
 </script>
